221b2672560fedefce476fda2827ce5bffa756a5
Blessing U <blessing986@users.noreply.github.com>
2024-04-10 15:44:49 +0100

    chore: hiding functionality using private modifiers

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,7 @@
 // const company = new Company();
 // console.log(company);
 
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+import { CustomMap } from "./CustomMap";
+
+new CustomMap("map");
+
