dfcf2ff6609c5346f08fbf49673442fa903b0be1
Blessing U <blessing986@users.noreply.github.com>
2024-04-16 16:03:38 +0100

    chore: added click event listener to display infoWindow on the marker.

--- src/User.ts
+++ src/User.ts
@@ -6,11 +6,16 @@ export class User {
     lat: number;
     lng: number;
   };
+
   constructor() {
     this.name = faker.person.firstName();
     this.location = {
       lat: faker.location.latitude(),
       lng: faker.location.longitude(),
     };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
